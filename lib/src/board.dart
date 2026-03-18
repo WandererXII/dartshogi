e96bda4e0e5903a4cddde7d1ed3721d8832af4b8
WandererXII <43043181+WandererXII@users.noreply.github.com>
2026-03-18 23:25:29 +0100

    Board

--- lib/src/board.dart
+++ lib/src/board.dart
@@ -2,180 +2,36 @@ import 'package:meta/meta.dart';
 import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import './square_set.dart';
 import './models.dart';
-import './attacks.dart';
 
 /// A board represented by several square sets for each piece.
 @immutable
 class Board {
   const Board({
     required this.occupied,
-    required this.promoted,
-    required this.white,
-    required this.black,
-    required this.pawns,
-    required this.knights,
-    required this.bishops,
-    required this.rooks,
-    required this.queens,
-    required this.kings,
+    required this.sente,
+    required this.gote,
+    required this.roles,
   });
 
   /// All occupied squares.
   final SquareSet occupied;
 
-  /// All squares occupied by pieces known to be promoted.
-  ///
-  /// This information is relevant in chess variants like [Crazyhouse].
-  final SquareSet promoted;
+  /// All squares occupied by sente pieces.
+  final SquareSet sente;
 
-  /// All squares occupied by white pieces.
-  final SquareSet white;
+  /// All squares occupied by gote pieces.
+  final SquareSet gote;
 
-  /// All squares occupied by black pieces.
-  final SquareSet black;
-
-  /// All squares occupied by pawns.
-  final SquareSet pawns;
-
-  /// All squares occupied by knights.
-  final SquareSet knights;
-
-  /// All squares occupied by bishops.
-  final SquareSet bishops;
-
-  /// All squares occupied by rooks.
-  final SquareSet rooks;
-
-  /// All squares occupied by queens.
-  final SquareSet queens;
-
-  /// All squares occupied by kings.
-  final SquareSet kings;
-
-  /// Standard chess starting position.
-  static const standard = Board(
-    occupied: SquareSet(0xffff00000000ffff),
-    promoted: SquareSet.empty,
-    white: SquareSet(0xffff),
-    black: SquareSet(0xffff000000000000),
-    pawns: SquareSet(0x00ff00000000ff00),
-    knights: SquareSet(0x4200000000000042),
-    bishops: SquareSet(0x2400000000000024),
-    rooks: SquareSet.corners,
-    queens: SquareSet(0x0800000000000008),
-    kings: SquareSet(0x1000000000000010),
-  );
-
-  /// Racing Kings start position
-  static const racingKings = Board(
-      occupied: SquareSet(0xffff),
-      promoted: SquareSet.empty,
-      white: SquareSet(0xf0f0),
-      black: SquareSet(0x0f0f),
-      pawns: SquareSet.empty,
-      knights: SquareSet(0x1818),
-      bishops: SquareSet(0x2424),
-      rooks: SquareSet(0x4242),
-      queens: SquareSet(0x0081),
-      kings: SquareSet(0x8100));
-
-  /// Horde start Positioin
-  static const horde = Board(
-    occupied: SquareSet(0xffff0066ffffffff),
-    promoted: SquareSet.empty,
-    white: SquareSet(0x00000066ffffffff),
-    black: SquareSet(0xffff000000000000),
-    pawns: SquareSet(0x00ff0066ffffffff),
-    knights: SquareSet(0x4200000000000000),
-    bishops: SquareSet(0x2400000000000000),
-    rooks: SquareSet(0x8100000000000000),
-    queens: SquareSet(0x0800000000000000),
-    kings: SquareSet(0x1000000000000000),
-  );
+  /// Map of all roles and squares they occupy.
+  final IMap<Role, SquareSet> roles;
 
   /// Empty board.
-  static const empty = Board(
+  static final empty = Board(
     occupied: SquareSet.empty,
-    promoted: SquareSet.empty,
-    white: SquareSet.empty,
-    black: SquareSet.empty,
-    pawns: SquareSet.empty,
-    knights: SquareSet.empty,
-    bishops: SquareSet.empty,
-    rooks: SquareSet.empty,
-    queens: SquareSet.empty,
-    kings: SquareSet.empty,
+    sente: SquareSet.empty,
+    gote: SquareSet.empty,
+    roles: const IMap.empty(),
   );
-
-  /// Parse the board part of a FEN string and returns a Board.
-  ///
-  /// Throws a [FenException] if the provided FEN string is not valid.
-  factory Board.parseFen(String boardFen) {
-    Board board = Board.empty;
-    int rank = 7;
-    int file = 0;
-    for (int i = 0; i < boardFen.length; i++) {
-      final c = boardFen[i];
-      if (c == '/' && file == 8) {
-        file = 0;
-        rank--;
-      } else {
-        final code = c.codeUnitAt(0);
-        if (code < 57) {
-          file += code - 48;
-        } else {
-          if (file >= 8 || rank < 0) {
-            throw const FenException(IllegalFenCause.board);
-          }
-          final square = Square(file + rank * 8);
-          final promoted = i + 1 < boardFen.length && boardFen[i + 1] == '~';
-          final piece = _charToPiece(c, promoted);
-          if (piece == null) throw const FenException(IllegalFenCause.board);
-          if (promoted) i++;
-          board = board.setPieceAt(square, piece);
-          file++;
-        }
-      }
-    }
-    if (rank != 0 || file != 8) throw const FenException(IllegalFenCause.board);
-    return board;
-  }
-
-  /// The square set of all rooks and queens.
-  SquareSet get rooksAndQueens => rooks | queens;
-
-  /// The square set of all bishops and queens.
-  SquareSet get bishopsAndQueens => bishops | queens;
-
-  /// Board part of the Forsyth-Edwards-Notation.
-  String get fen {
-    final buffer = StringBuffer();
-    int empty = 0;
-    for (int rank = 7; rank >= 0; rank--) {
-      for (int file = 0; file < 8; file++) {
-        final square = Square(file + rank * 8);
-        final piece = pieceAt(square);
-        if (piece == null) {
-          empty++;
-        } else {
-          if (empty > 0) {
-            buffer.write(empty.toString());
-            empty = 0;
-          }
-          buffer.write(piece.fenChar);
-        }
-
-        if (file == 7) {
-          if (empty > 0) {
-            buffer.write(empty.toString());
-            empty = 0;
-          }
-          if (rank != 0) buffer.write('/');
-        }
-      }
-    }
-    return buffer.toString();
-  }
 
   /// An iterable of each [Piece] associated to its [Square].
   Iterable<(Square, Piece)> get pieces sync* {
@@ -194,24 +50,11 @@ class Board {
   }
 
   /// Gets all squares occupied by [Side].
-  SquareSet bySide(Side side) => side == Side.white ? white : black;
+  SquareSet bySide(Side side) => side == Side.sente ? sente : gote;
 
   /// Gets all squares occupied by [Role].
   SquareSet byRole(Role role) {
-    switch (role) {
-      case Role.pawn:
-        return pawns;
-      case Role.knight:
-        return knights;
-      case Role.bishop:
-        return bishops;
-      case Role.rook:
-        return rooks;
-      case Role.queen:
-        return queens;
-      case Role.king:
-        return kings;
-    }
+    return roles[role] ?? SquareSet.empty;
   }
 
   /// Gets all squares occupied by [Piece].
@@ -221,10 +64,10 @@ class Board {
 
   /// Gets the [Side] at this [Square], if any.
   Side? sideAt(Square square) {
-    if (bySide(Side.white).has(square)) {
-      return Side.white;
-    } else if (bySide(Side.black).has(square)) {
-      return Side.black;
+    if (bySide(Side.sente).has(square)) {
+      return Side.sente;
+    } else if (bySide(Side.gote).has(square)) {
+      return Side.gote;
     } else {
       return null;
     }
@@ -232,9 +75,11 @@ class Board {
 
   /// Gets the [Role] at this [Square], if any.
   Role? roleAt(Square square) {
-    for (final role in Role.values) {
-      if (byRole(role).has(square)) {
-        return role;
+    if (!occupied.has(square)) return null;
+
+    for (final entry in roles.entries) {
+      if (entry.value.has(square)) {
+        return entry.key;
       }
     }
     return null;
@@ -247,38 +92,25 @@ class Board {
       return null;
     }
     final role = roleAt(square)!;
-    final prom = promoted.has(square);
-    return Piece(color: side, role: role, promoted: prom);
+    return Piece(color: side, role: role);
   }
 
   /// Finds the unique king [Square] of the given [Side], if any.
   Square? kingOf(Side side) {
-    return byPiece(Piece(color: side, role: Role.king)).singleSquare;
+    return byPiece(Piece(color: side, role: Role.king)).singleSquare();
   }
-
-  /// Finds the squares who are attacking `square` by the `attacker` [Side].
-  SquareSet attacksTo(Square square, Side attacker, {SquareSet? occupied}) =>
-      bySide(attacker).intersect(rookAttacks(square, occupied ?? this.occupied)
-          .intersect(rooksAndQueens)
-          .union(bishopAttacks(square, occupied ?? this.occupied)
-              .intersect(bishopsAndQueens))
-          .union(knightAttacks(square).intersect(knights))
-          .union(kingAttacks(square).intersect(kings))
-          .union(pawnAttacks(attacker.opposite, square).intersect(pawns)));
 
   /// Puts a [Piece] on a [Square] overriding the existing one, if any.
   Board setPieceAt(Square square, Piece piece) {
     return removePieceAt(square).copyWith(
       occupied: occupied.withSquare(square),
-      promoted: piece.promoted ? promoted.withSquare(square) : null,
-      white: piece.color == Side.white ? white.withSquare(square) : null,
-      black: piece.color == Side.black ? black.withSquare(square) : null,
-      pawns: piece.role == Role.pawn ? pawns.withSquare(square) : null,
-      knights: piece.role == Role.knight ? knights.withSquare(square) : null,
-      bishops: piece.role == Role.bishop ? bishops.withSquare(square) : null,
-      rooks: piece.role == Role.rook ? rooks.withSquare(square) : null,
-      queens: piece.role == Role.queen ? queens.withSquare(square) : null,
-      kings: piece.role == Role.king ? kings.withSquare(square) : null,
+      sente: piece.color == Side.sente ? sente.withSquare(square) : null,
+      gote: piece.color == Side.gote ? gote.withSquare(square) : null,
+      roles: roles.update(
+        piece.role,
+        (sqs) => sqs.withSquare(square),
+        ifAbsent: () => SquareSet.empty,
+      ),
     );
   }
 
@@ -288,89 +120,46 @@ class Board {
     return piece != null
         ? copyWith(
             occupied: occupied.withoutSquare(square),
-            promoted: piece.promoted ? promoted.withoutSquare(square) : null,
-            white:
-                piece.color == Side.white ? white.withoutSquare(square) : null,
-            black:
-                piece.color == Side.black ? black.withoutSquare(square) : null,
-            pawns: piece.role == Role.pawn ? pawns.withoutSquare(square) : null,
-            knights: piece.role == Role.knight
-                ? knights.withoutSquare(square)
-                : null,
-            bishops: piece.role == Role.bishop
-                ? bishops.withoutSquare(square)
-                : null,
-            rooks: piece.role == Role.rook ? rooks.withoutSquare(square) : null,
-            queens:
-                piece.role == Role.queen ? queens.withoutSquare(square) : null,
-            kings: piece.role == Role.king ? kings.withoutSquare(square) : null,
+            sente:
+                piece.color == Side.sente ? sente.withoutSquare(square) : null,
+            gote: piece.color == Side.gote ? gote.withoutSquare(square) : null,
+            roles: roles.update(
+              piece.role,
+              (sqs) => sqs.withoutSquare(square),
+              ifAbsent: () => SquareSet.empty,
+            ),
           )
         : this;
-  }
-
-  /// Returns a new board with a new [promoted] square set.
-  Board withPromoted(SquareSet promoted) {
-    return copyWith(promoted: promoted);
   }
 
   /// Returns a copy of this board with some fields updated.
   Board copyWith({
     SquareSet? occupied,
-    SquareSet? promoted,
-    SquareSet? white,
-    SquareSet? black,
-    SquareSet? pawns,
-    SquareSet? knights,
-    SquareSet? bishops,
-    SquareSet? rooks,
-    SquareSet? queens,
-    SquareSet? kings,
+    SquareSet? sente,
+    SquareSet? gote,
+    IMap<Role, SquareSet>? roles,
   }) {
     return Board(
       occupied: occupied ?? this.occupied,
-      promoted: promoted ?? this.promoted,
-      white: white ?? this.white,
-      black: black ?? this.black,
-      pawns: pawns ?? this.pawns,
-      knights: knights ?? this.knights,
-      bishops: bishops ?? this.bishops,
-      rooks: rooks ?? this.rooks,
-      queens: queens ?? this.queens,
-      kings: kings ?? this.kings,
+      sente: sente ?? this.sente,
+      gote: gote ?? this.gote,
+      roles: roles ?? this.roles,
     );
   }
 
-  @override
-  String toString() => fen;
+  // @override
+  // String toString() => sfen;
 
   @override
   bool operator ==(Object other) {
     return identical(this, other) ||
         other is Board &&
             other.occupied == occupied &&
-            other.promoted == promoted &&
-            other.white == white &&
-            other.black == black &&
-            other.pawns == pawns &&
-            other.knights == knights &&
-            other.bishops == bishops &&
-            other.rooks == rooks &&
-            other.queens == queens &&
-            other.kings == kings;
+            other.sente == sente &&
+            other.gote == gote &&
+            other.roles == roles;
   }
 
   @override
-  int get hashCode => Object.hash(occupied, promoted, white, black, pawns,
-      knights, bishops, rooks, queens, kings);
-}
-
-Piece? _charToPiece(String ch, bool promoted) {
-  final role = Role.fromChar(ch);
-  if (role != null) {
-    return Piece(
-        role: role,
-        color: ch == ch.toLowerCase() ? Side.black : Side.white,
-        promoted: promoted);
-  }
-  return null;
+  int get hashCode => Object.hash(occupied, sente, gote, roles);
 }
